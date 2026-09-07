4e7dffbecd29f89f0471ec98e7a5075333a928ce
ahad <engrabdulahad924@gmail.com>
2026-09-07 21:09:32 +0500

    added cron job

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const app = require('./app');
 const connectDatabase = require('./config/database');
 const env = require('./config/env');
+const { startKeepAlive } = require('./utils/keepAlive');
 
 const startServer = async () => {
   await connectDatabase();
 
   app.listen(env.port, () => {
     console.log(`ELK ERP server running on port ${env.port}`);
+    startKeepAlive(env);
   });
 };
 
